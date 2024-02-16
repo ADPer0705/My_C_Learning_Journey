d14f2551084f9f48bdfb8c3f9b40156e73a0de2a
ADPer0705 <142957292+ADPer0705@users.noreply.github.com>
2024-02-16 10:31:41 +0530

    starting over ems.c
    I'm starting this project over since it's been some time since I half started it

--- ems.c
+++ ems.c
@@ -1,31 +1,44 @@
+/**This is an Employee Management System in C,
+It has the following functionalities:
+1.) Add record (upto 100)
+2.) Find Record 
+3.) Display all records 
+4.) Edit record 
+5.) Delete record
+6.) Employee Statisticd 
+It is a menu driven application **/
+
+
 #include<stdio.h>
 #include<conio.h>
-//#include<array.h>
+#include<array.h>
 
 struct employees{
     char no[18];
     char name[50];
     char dept[10];
     float sal;
-}emp[10];
+}emp[100];
 
 void main(){
 
-    repeat:
-
-    for(int i=0; i++; i<10){
-        //emp[i].no={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    }
-
-    printf("Welcome to the employee management system.\n");
-    printf("Please enter the no next to the \ntask to be performed\n\n");
-    printf("1.) Add Record\n");
-    printf("2.) Edit record\n");
-    printf("3.) Add Record\n");
-    printf("4.) Add Record\n");
-    printf("5.) Add Record\n");
-    printf("6.) Add Record\n");
-    printf("7.) exit\n");
-    printf("task : ");
+    int choice;
+    do
+    {
+        for(int i=0; i++; i<10)
+        {
+            emp[i].no={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        }
     
+        printf("Welcome to the employee management system.\n");
+        printf("Please enter the no next to the \ntask to be performed\n\n");
+        printf("1.) Add Record\n");
+        printf("2.) Find Record\n");
+        printf("3.) Display all Record\n");
+        printf("4.) Edit Record\n");
+        printf("5.) Delete Record\n");
+        printf("6.) Employee Statistics\n");
+        printf("0.) exit\n");
+        printf("task : ");
+    }while(choice!=0);
 }
